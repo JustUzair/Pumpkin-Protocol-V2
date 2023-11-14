e4ad59b88147a12bf26ed849639fe5d6de2d266a
JustUzair <uzairhajra763390@gmail.com>
2023-11-14 19:37:14 +0530

    Fixed footer image placeholder

--- frontend/components/footer.tsx
+++ frontend/components/footer.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 export const Footer = () => {
   return (
     <div className="relative">
@@ -8,7 +9,7 @@ export const Footer = () => {
               href="https://flowbite.com/"
               className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
             >
-              <img
+              <Image
                 src="https://flowbite.com/docs/images/logo.svg"
                 className="h-8"
                 alt="Flowbite Logo"
